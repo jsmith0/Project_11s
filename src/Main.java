aaee0a931c4c6d9851e7f8e26f09c488d4e44c06
jsmith0 <justins4@comcast.net>
2017-03-22 18:26:30 -0400

    stuff got messy. but its all there.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         //I realize all my classes turned into a jumbled mess. I "refactored" something so i could insert the key and still have
         //stuff but then i had to rename it and it renamed ALL my my stuff.
         //the classes without "2A" are mine.
-        //so I did the stuff. the names just may be really jacked up for some things.
+        //so I did the stuff. the names just may be really jacked up for some  things.
     }
 }
